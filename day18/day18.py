a13de8489add0518f35224a6fecd6f3fc5509a5a
RizkyNs <128666539+RizkyNs@users.noreply.github.com>
2026-03-08 09:28:05 +0800

    Create day18.py
    Add Day 18 - Python List Practice

---
+++ day18/day18.py
@@ -0,0 +1,9 @@
+# Day 18 - List dasar di Python
+
+buah = ["apel", "jeruk", "mangga", "pisang"]
+
+print("Daftar buah:")
+for item in buah:
+    print("-", item)
+
+print("\nJumlah buah:", len(buah))
